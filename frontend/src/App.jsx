f9be3b7ac1b34cde40bc0e4a8374c487c8261a0c
Amber Hunt <amberhunt955@gmail.com>
2023-08-15 21:56:39 -0400

    initialized frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Chingu Voyage V45 Tier 3 Team 52</h1>
+    </>
+  );
+}
+
+export default App;
