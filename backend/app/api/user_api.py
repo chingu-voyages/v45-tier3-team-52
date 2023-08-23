0e308d9efff6dc5fc280d8ad9fcf3a6acd331aa9
Alexander Hunt <alexhunt215@gmail.com>
2023-08-22 22:27:12 -0400

    relocated the api, models, seeds files within the app directory

--- backend/app/api/user_api.py
+++ backend/app/api/user_api.py
@@ -1,15 +1,20 @@
 from flask import request, Blueprint
-from flask_login import login_required, current_user 
-from backend.models import User, db
+from flask_login import login_required, current_user
+from app.models import User, db
 
 user_routes = Blueprint("users", __name__)
 auth_error = "User not authorized to complete this action"
 
-@user_routes.route('/<int:id>')
+###### * Get User #################################
 
+
+@user_routes.route('/<int:id>')
 def user(id):
     user = User.query.get_or_404(id)
     return user.to_dict()
+
+###### * Delete User #################################
+
 
 @user_routes.route('/<int:id>', methods=["DELETE"])
 @login_required
@@ -17,12 +22,15 @@ def user_delete(id):
     queried_user = User.query.get_or_404(id)
 
     if queried_user.id != current_user.id:
-        return auth_error 
+        return auth_error
     else:
         db.session.delete(queried_user)
         db.session.commit()
         return {"message": "Successfully delete"}
-    
+
+###### * Update User #################################
+
+
 @user_routes.route('/<int:id>', methods=["PUT"])
 @login_required
 def user_edit(id):
@@ -30,14 +38,11 @@ def user_edit(id):
     queried_user = User.query.get_or_404(id)
 
     if queried_user.id != current_user.id:
-        return auth_error 
+        return auth_error
     else:
-        req_data = request.json 
+        req_data = request.json
         for key, val in req_data.items():
             if key != None:
                 setattr(queried_user, key, val)
         db.session.commit()
         return queried_user.to_dict()
-
-
-
