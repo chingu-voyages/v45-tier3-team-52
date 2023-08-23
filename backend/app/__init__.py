0f07cc6ebc5bdb569b41c983c79b7efb46b31157
Winnie <winnieng3210@gmail.com>
2023-08-23 19:24:26 -0400

    add create session forms, set up user auth routes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,6 +8,8 @@ from .models import User, db
 from .api.user_api import user_routes
 from .seeds import seed_commands
 from .config import Config
+from .api.auth_api import auth_routes
+from .api.user_api import user_routes
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
@@ -28,6 +30,8 @@ app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
+app.register_blueprint(auth_routes, url_prefix='/api/auth')
+
 db.init_app(app)
 Migrate(app, db)
 
