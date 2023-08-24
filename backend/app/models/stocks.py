ce83e97e39671e787370fd3efdfb54893f13df47
Alexander Hunt <alexhunt215@gmail.com>
2023-08-23 23:20:40 -0400

    addition of relationships 'portfolio_owner, portfolio_stock' to user_portfolio directory, addition of relationships 'stock_portfolio, stock_transactions' to stocks directory

--- backend/app/models/stocks.py
+++ backend/app/models/stocks.py
@@ -1,37 +1,42 @@
 from .db import db, environment, SCHEMA
 from datetime import datetime
+from sqlalchemy.sql import func
 from .portfolio_stock import portfolio_stocks
 
-class Stock(db.Model):
-    __tablename__='stocks'
-    if environment == "production":
-        __table_args__ = {'schema' : SCHEMA}
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(255),nullable=False)
-    symbol=db.Column(db.String(255), nullable=False, unique=True)
-    abv=db.Column(db.String(255))
-    currentPrice=db.Column(db.Integer)
 
-    portfolios=db.relationship('Portfolio', secondary=portfolio_stocks,back_populates='stocks')
-   
-    createdAt = db.Column(db.DateTime(
+class Stock(db.Model):
+    __tablename__ = 'stocks'
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    symbol = db.Column(db.String(255), nullable=False, unique=True)
+    abv = db.Column(db.String(255))
+    currentPrice = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(
         timezone=True), server_default=func.now())
-    updatedAt = db.Column(
+    updated_at = db.Column(
         db.DateTime,
         default=datetime.utcnow,
         onupdate=datetime.utcnow,
         nullable=False
     )
-    
+
+    # ! Relationships
+    stock_portfolio = db.relationship(
+        'user_portfolios', secondary=portfolio_stocks, back_populates='portfolio_stock')
+    stock_transactions = db.relationship(
+        'transactions', back_populates='transactions_stock')
+
+    # ? Methods
     def to_dict(self):
         return {
-            'id':self.id,
-            'name':self.name,
-            'symbol':self.symbol,
-            'abv':self.abv,
-            'currentPrice':self.currentPrice,
-            'createdAt':self.createdAt,
-            'updatedAt':self.updatedAt,
-            'portfolios':self.portfolios.to_dict()
-
+            'id': self.id,
+            'name': self.name,
+            'symbol': self.symbol,
+            'abv': self.abv,
+            'currentPrice': self.currentPrice,
+            'createdAt': self.createdAt,
+            'updatedAt': self.updatedAt,
+            'portfolio': self.stock_portfolio.to_dict()
         }
--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -2,6 +2,7 @@ from .db import db, environment, SCHEMA
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -16,16 +17,22 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     wallet = db.Column(db.Float(100))
     hashed_password = db.Column(db.String(255), nullable=False)
-    created_date = db.Column(db.DateTime(
+    created_at = db.Column(db.DateTime(
         timezone=True), server_default=func.now())
-    updated_date = db.Column(db.DateTime(timezone=True),
-                             server_default=func.now(), onupdate=func.now())
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
 
     # ! Relationships
     owner_name = db.relationship(
-        'user_portfolio', back_populates="name_owner", cascade="all, delete")
+        'user_portfolios', back_populates='name_owner', cascade='all, delete')
     owner_transactions = db.relationship(
-        'transactions', back_populates="transactions_owner", cascade="all, delete")
+        'transactions', back_populates='transactions_owner', cascade='all, delete')
+    owner_portfolio = db.relationship(
+        'user_portfolios', back_populates='portfolio_owner', cascade='all, delete')
 
     # ? Methods
     @property
