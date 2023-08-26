c6e03cb0f27aa6b6bc25e8bf59ccedfc31de4685
Winnie <winnieng3210@gmail.com>
2023-08-26 13:00:44 -0400

    routes refactored

--- backend/app/api/auth_api.py
+++ backend/app/api/auth_api.py
@@ -1,29 +1,28 @@
-from flask import Blueprint, redirect, url_for, request
+from flask import Blueprint, redirect, url_for, request, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, db 
+from app.models import User, db
 from .forms import LoginForm, RegisterForm
+
 
 auth_routes = Blueprint("auth", __name__)
 
-@auth_routes.route("/login", methods=['GET', 'POST'])
+@auth_routes.route("/login", methods=['POST'])
 def login():
     form = LoginForm()
-
+    form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
         # if bcrypt.check_password_hash(user.password, form.password.data)
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
-        # return user.to_dict()
-        return {"message": "login works"}
+        return user.to_dict()
+    return {'error': 'failed to log in'}, 401
 
 
 @auth_routes.route("/register", methods=['POST'])
 def register():
     form = RegisterForm()
-
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        print("on validate")
         new_user = User(
             first_name=form.data['first_name'],
             last_name=form.data['last_name'],
@@ -33,16 +32,11 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
-        # return new_user.to_dict()
-        return {"message": "register works"}
-    else:
-        print("not validate")
-    
+        return new_user.to_dict()
+    return {'error': 'registration failed'}, 401
+
 @auth_routes.route("/logout")
 def logout():
     logout_user()
-    # redirect(url_for("login"))
     return {"message": "logout works"}
-    
-
 
