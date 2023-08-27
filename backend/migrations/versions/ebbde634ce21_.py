3f8498d49e0bf69d140da6dc804b0f2da23166a7
Winnie <winnieng3210@gmail.com>
2023-08-27 10:53:04 -0400

    tablet style

--- backend/migrations/versions/ebbde634ce21_.py
+++ backend/migrations/versions/ebbde634ce21_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 41f0ac30f482
+Revision ID: ebbde634ce21
 Revises: 
-Create Date: 2023-08-24 21:25:32.224538
+Create Date: 2023-08-27 16:08:48.718918
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '41f0ac30f482'
+revision = 'ebbde634ce21'
 down_revision = None
 branch_labels = None
 depends_on = None
--- frontend/src/components/Auth/auth_form.jsx
+++ frontend/src/components/Auth/auth_form.jsx
@@ -5,6 +5,8 @@ const textInputStyle = "text-xs";
 const inputStyle =
 	"border-solid border-gray-300 border w-full mt-2 py-1 px-2 rounded text-black";
 
+const authDesktopStyle = "md:flex";
+
 const AuthForm = props => {
 	const { formType } = props;
 	const navigate = useNavigate();
@@ -14,8 +16,8 @@ const AuthForm = props => {
 	const [lastName, setLastName] = useState("");
 
 	const registerName = (
-		<>
-			<div>
+		<div className="flex flex-col gap-5 md:flex-row">
+			<div className="w-full">
 				<label htmlFor="first-name">
 					<div className={textInputStyle}>First Name</div>
 				</label>
@@ -28,7 +30,7 @@ const AuthForm = props => {
 					className={inputStyle}
 				/>
 			</div>
-			<div>
+			<div className="w-full">
 				<label htmlFor="last-name">
 					<div className={textInputStyle}>Last Name</div>
 				</label>
@@ -41,7 +43,7 @@ const AuthForm = props => {
 					className={inputStyle}
 				/>
 			</div>
-		</>
+		</div>
 	);
 
 	const sessionDisclaimer = (
@@ -52,63 +54,67 @@ const AuthForm = props => {
 	);
 
 	return (
-		<div className="h-full flex items-center justify-center bg-black text-white">
-			<div className="flex flex-col gap-5 mx-10 w-full">
-				<div className="text-xl">
-					{formType === "Register" ? "Sign up" : "Log in"} to Robinhood
-				</div>
-				<form className="flex flex-col gap-5">
-					{formType === "Register" && registerName}
-					<div>
-						<label htmlFor="email-address">
-							<div className={textInputStyle}>Email address</div>
-						</label>
-						<input
-							id="email-address"
-							type="text"
-							value={email}
-							onChange={e => setEmail(e.target.value)}
-							required
-							className={inputStyle}
-						/>
+		<div className={`h-full`}>
+			<div className="h-full flex items-center justify-center">
+				<div className="flex flex-col gap-5 mx-10 w-full md:mx-20">
+					<div className="text-xl">
+						{formType === "Register" ? "Sign up" : "Log in"} to Robinhood
 					</div>
-					<div>
-						<label htmlFor="password">
-							<div className={textInputStyle}>Password</div>
-						</label>
-						<input
-							id="password"
-							type="password"
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-							required
-							className={inputStyle}
-						/>
-					</div>
-				</form>
-				<button className="my-5 border-solid border-black-500 border w-full h-10 rounded-full bg-black text-white hover:cursor-pointer hover:bg-gray-300 hover:border-gray-300">
-					<div className="text-xs">
-						{formType === "Register" ? "Register" : "Log in"}
-					</div>
-				</button>
-				{/* {formType === "Log in" && } */}
-				<div className="flex gap-1 text-xs">
-					<div>
-						{formType === "Register" ? "Have an account?" : "Not on Robinhood?"}
-					</div>
-					<button
-						className="hover:cursor-pointer"
-						onClick={() =>
-							formType === "Register"
-								? navigate("/login")
-								: navigate("/register")
-						}>
-						<div className="font-bold border-b-2 border-white">
-							{formType === "Register" ? "Log in" : "Create an account"}
+					<form className="flex flex-col gap-5">
+						{formType === "Register" && registerName}
+						<div>
+							<label htmlFor="email-address">
+								<div className={textInputStyle}>Email address</div>
+							</label>
+							<input
+								id="email-address"
+								type="text"
+								value={email}
+								onChange={e => setEmail(e.target.value)}
+								required
+								className={inputStyle}
+							/>
+						</div>
+						<div>
+							<label htmlFor="password">
+								<div className={textInputStyle}>Password</div>
+							</label>
+							<input
+								id="password"
+								type="password"
+								value={password}
+								onChange={e => setPassword(e.target.value)}
+								required
+								className={inputStyle}
+							/>
+						</div>
+					</form>
+					<button className="my-5 border-solid border-black-500 border w-full h-10 rounded-full bg-black text-white hover:cursor-pointer hover:bg-gray-300 hover:border-gray-300">
+						<div className="text-xs">
+							{formType === "Register" ? "Register" : "Log in"}
 						</div>
 					</button>
+					{/* {formType === "Log in" && } */}
+					<div className="flex gap-1 text-xs">
+						<div>
+							{formType === "Register"
+								? "Have an account?"
+								: "Not on Robinhood?"}
+						</div>
+						<button
+							className="hover:cursor-pointer"
+							onClick={() =>
+								formType === "Register"
+									? navigate("/login")
+									: navigate("/register")
+							}>
+							<div className="font-bold border-b-2 border-black">
+								{formType === "Register" ? "Log in" : "Create an account"}
+							</div>
+						</button>
+					</div>
+					{formType === "Register" && sessionDisclaimer}
 				</div>
-				{formType === "Register" && sessionDisclaimer}
 			</div>
 		</div>
 	);
