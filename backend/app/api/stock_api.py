c63fa6c510443d08f2aa6077e367248030af952b
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2023-08-31 16:46:03 -0400

    Update stock api and seed file; api data only, update ticker list function to include more stocks

--- backend/app/api/stock_api.py
+++ backend/app/api/stock_api.py
@@ -4,10 +4,11 @@ import os
 import urllib.request
 import json
 import yfinance as yf
+# import func, or
 
 stock_route = Blueprint("stocks",  __name__)
 
-polygon_API = "https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-08-25?adjusted=true&apiKey={}".format(
+polygon_API = "https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-08-25?adjusted=true&sort=desc&apiKey={}".format(
     os.getenv('API_KEY'))
 
 
@@ -21,7 +22,7 @@ def api_data(url):
 def ticker_list(stocks):
     newlist = []
     count = 0
-    while count <= 60:
+    while count <= 200:
         newlist.append(
             {"ticker_name": stocks[count]["T"], "close_price": stocks[count]['c']})
         count += 1
@@ -31,12 +32,15 @@ def ticker_list(stocks):
 def convert_symbol_to_company(ticker_list):
     stock_list = []
     for ticker in ticker_list:
-        current_ticker = yf.Ticker(ticker['ticker_name'])
-        stock_list.append({
-            'symbol': ticker['ticker_name'],
-            "org_name": current_ticker.info['longName'],
-            "current_price": ticker['close_price']
-        })
+        if (not ticker['ticker_name'].isupper()):
+            continue
+        else:
+            current_ticker = yf.Ticker(ticker['ticker_name'])
+            stock_list.append({
+                'symbol': ticker['ticker_name'],
+                "org_name": current_ticker.info['longName'],
+                "current_price": ticker['close_price']
+            })
     return stock_list
 
 
@@ -45,10 +49,27 @@ def list_of_stock():
     return api_data(polygon_API)
 
 
-@stock_route.route('/<int:id>')
-def stock(id):
-    stock = Stock.query.get_or_404(id)
-    return stock.to_dict()
+# @stock_route.route('/<>', methods=['GET'])
+# def get_stock(value):
+#     """
+#     Route allowing user to query stock by id, symbol, or orgname.
+#     """
+#     stock = None
+
+#     # Try querying by ID
+#     if value.isdigit():
+#         stock = Stock.query.get(int(value))
+
+#     # If not found by ID, try querying by symbol (case-insensitive)
+#     if stock is None:
+#         stock = Stock.query.filter(or(
+#             func.lower(Stock.symbol) == value.lower(),
+#             func.lower(Stock.org_name) == value.lower()
+#         )).first()
+#     if stock is None:
+#         return jsonify({'error': 'Stock not found'}), 404
+
+#     return stock.to_dict()
 
 
 @stock_route.route('/addstock', methods=["POST"])
@@ -56,19 +77,21 @@ def add_stocks():
     db_stocks = []
     aggs = convert_symbol_to_company(ticker_list(api_data(polygon_API)))
     for stock_currated in aggs:
-        all_stocks = db.session.query(Stock.id).filter_by(
-            symbol=stock_currated['symbol']).first() is not None
-        if all_stocks == True:
-            print("Already in DB")
-        elif all_stocks == False:
-            new_stock = Stock(
-                org_name=stock_currated['org_name'],
-                symbol=stock_currated['symbol'],
-                current_price=stock_currated['current_price']
-            )
-            db_stocks.append(new_stock)
-            db.session.add(new_stock)
-            db.session.commit()
-        else:
-            print("Stock Symbol Not Found")
-    return [stock_db.to_dict() for stock_db in db_stocks], {'message': "Sucess 200"}
+        # all_stocks = db.session.query(Stock.id).filter_by(
+        #     symbol=stock_currated['symbol']).first() is not None
+        # if all_stocks == True:
+        #     print("Already in DB")
+        # elif all_stocks == False:
+        new_stock = Stock(
+            org_name=stock_currated['org_name'],
+            symbol=stock_currated['symbol'],
+            current_price=stock_currated['current_price']
+        )
+        db_stocks.append(new_stock)
+    db.session.add_all(db_stocks)
+    db.session.commit()
+    # db.session.add(new_stock)
+    # db.session.commit()
+    # else:
+    #     print("Stock Symbol Not Found")
+    # return [stock_db.to_dict() for stock_db in db_stocks], {'message': "Sucess 200"}
